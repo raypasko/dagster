2def4d632493df56e0fbecaa023e9136df698c1e
Max Gasner <max.gasner@gmail.com>
2019-10-11 17:04:28 -0700

    Improve type inference
    Summary:
    Previously, it was impossible to create Dagster types directly wrapping
    built-ins and extensions, because we attempted to monkeypatch the Python type
    with an annotation pointing to the Dagster type. Additionally, attempting to
    annotate a solid with a Python type outside of a narrow set would result in an
    unexpected error, if that type were not explicitly wrapped in as_dagster_type or
    equivalent. This diff replaces the monkeypatching machinery with an explicit
    runtime type registry, and also adds a facility for automatically wrapping bare
    Python types in anonymous Dagster types.
    
    Test Plan: Unit
    
    Differential Revision: https://dagster.phacility.com/D1273

--- python_modules/dagster/dagster/core/types/runtime.py
+++ python_modules/dagster/dagster/core/types/runtime.py
@@ -278,7 +278,7 @@ class Nothing(RuntimeType):
 
 
 class PythonObjectType(RuntimeType):
-    def __init__(self, python_type, key=None, name=None, typecheck_metadata_fn=None, **kwargs):
+    def __init__(self, python_type=None, key=None, name=None, typecheck_metadata_fn=None, **kwargs):
         name = check.opt_str_param(name, 'name', type(self).__name__)
         key = check.opt_str_param(key, 'key', name)
         super(PythonObjectType, self).__init__(key=key, name=name, **kwargs)
@@ -300,6 +300,9 @@ class PythonObjectType(RuntimeType):
         if self.typecheck_metadata_fn:
             return self.typecheck_metadata_fn(value)
 
+    # def __call__(self, *args, **kwargs):
+    #     return self.python_type(*args, **kwargs)
+
 
 def define_python_dagster_type(
     python_type,
@@ -311,17 +314,17 @@ def define_python_dagster_type(
     auto_plugins=None,
     typecheck_metadata_fn=None,
 ):
-    '''
-    The dagster typesystem is very flexible, and the body of a typecheck can be
-    a function that does *anything*. (For that level of flexiblity one should inherit
-    from RuntimeType directly)  However its very common to want to generate a dagster
-    type whose only typecheck is against a python type:
+    '''Define a dagster type corresponding to an existing python type.
 
-    DateTime = define_python_dagster_type(datetime.datetime, name='DateTime')
+    It's very common to want to generate a dagster type corresponding to an existing Python type.
+    Typically this is done using the @dagster_type decorator or using as_dagster_type, each of
+    which defer to this function as a workhorse.
+
+        DateTime = define_python_dagster_type(datetime.datetime, name='DateTime')
 
     Args:
         python_type (cls)
-            The python type you want check against.
+            The python type against which you want to check types.
         name (Optional[str]):
             Name of the dagster type. Defaults to the name of the python_type.
         description (Optiona[str]):
@@ -540,7 +543,6 @@ _RUNTIME_MAP = {
 
 def resolve_to_runtime_type(dagster_type):
     # circular dep
-    from .decorator import is_runtime_type_decorated_klass, get_runtime_type_on_decorated_klass
     from .mapping import remap_python_type
     from .python_dict import PythonDict, create_typed_runtime_dict
     from .python_set import PythonSet, create_typed_runtime_set
@@ -575,8 +577,6 @@ def resolve_to_runtime_type(dagster_type):
         return resolve_to_runtime_list(dagster_type)
     if isinstance(dagster_type, WrappingNullableType):
         return resolve_to_runtime_nullable(dagster_type)
-    if is_runtime_type_decorated_klass(dagster_type):
-        return get_runtime_type_on_decorated_klass(dagster_type)
     if issubclass(dagster_type, RuntimeType):
         return dagster_type.inst()
 
